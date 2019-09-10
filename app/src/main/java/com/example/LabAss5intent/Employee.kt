9338de5ed737be6a4043f89c2eb3551f066e0889
gerkku <54529272+gerkku@users.noreply.github.com>
2019-09-11 01:12:36 +0700

    ASS5_603021730-2

---
+++ app/src/main/java/com/example/LabAss5intent/Employee.kt
@@ -0,0 +1,12 @@
+package com.example.LabAss5intent
+
+
+
+import  android.os.Parcelable
+import  kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Employee (val name: String, val gender:String, val email: String, val salary: Int) : Parcelable{
+
+
+}
